b8fdd7cf783696022db98cc47b5facf5fcfb55d8
Allan Binayao <allan.binayao@gmail.com>
2020-04-04 11:10:28 -0500

    added environmental variables

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // Requiring necessary npm packages
 var express = require("express");
 var session = require("express-session");
-// Requiring passport as we've configured it
+// Requiring passport as we've configured it 
 var passport = require("./config/passport");
-
+var dotenv = require("dotenv")
+dotenv.config();
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
 var db = require("./models");
