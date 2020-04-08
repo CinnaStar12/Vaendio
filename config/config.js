d8cbe126bd0d01d9aef070c8ade7c36bfe933037
7-Spades <edwin5106@gmail.com>
2020-04-07 19:50:00 -0500

    updated repo

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ module.exports = {
   "development": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
-    "database": "project_vendor",
+    "database": "passport_demo",
     "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
