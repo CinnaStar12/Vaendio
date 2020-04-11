842386df8231b0c8fbb27e4adfa77b43147f805f
philipwking <42879808+philipwking@users.noreply.github.com>
2020-04-11 11:52:50 -0500

    stylin!

--- public/js/map.js
+++ public/js/map.js
@@ -28,10 +28,8 @@ function initMap() {
   ]
 
   for (var i = 0; i < data.length; i++) {
-
-
     var icon = {
-      url: "/assets/vendor.png",
+      url: "./assets/vendor.png",
       scaledSize: new google.maps.Size(30, 30),
       origin: new google.maps.Point(0, 0),
       anchor: new google.maps.Point(0, 0)
@@ -47,54 +45,49 @@ function initMap() {
       $("#overlay").css("display","block")
       console.log("click")
     })
-    // replace window content with overlay
 
     markersArray.push(marker);
   }
 
 }
+
 function closeOverlay(){
   $("#overlay").css("display","none")
 }
 
-$(document).ready(function() {
-    $.get("/api/vendors").then(function (data) {
-        console.log(data);
-        for (i = 0; i < data.length; i++) {
-          var latLng = new google.maps.LatLng(data[i].latitude, data[i].longitude);
-          var icon = {
-            url: "", // insert image url
-            scaledSize: new google.maps.Size(30, 30),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(0, 0)
-          };
-          var marker = new google.maps.Marker({
-            position: latLng,
-            map: map,
-            icon: icon,
-            index: i
-          })
+// $(document).ready(function() {
+//     $.get("/api/vendors").then(function (data) {
+//         console.log(data);
+//         for (i = 0; i < data.length; i++) {
+//           var latLng = new google.maps.LatLng(data[i].latitude, data[i].longitude);
+//           var icon = {
+//             url: "", // insert image url
+//             scaledSize: new google.maps.Size(30, 30),
+//             origin: new google.maps.Point(0, 0),
+//             anchor: new google.maps.Point(0, 0)
+//           };
+//           var marker = new google.maps.Marker({
+//             position: latLng,
+//             map: map,
+//             icon: icon,
+//             index: i
+//           })
     
-          var infowindow = new google.maps.InfoWindow();
-          var windowContent = "" // html to go into window
+//           var infowindow = new google.maps.InfoWindow();
+//           var windowContent = "" // html to go into window
           
-          windowsArray.push(windowContent)
+//           windowsArray.push(windowContent)
     
-          google.maps.event.addListener(marker, "click", (function (marker) {
-            return function () {
-              console.log(windowsArray[marker.index])
-              var content = windowsArray[marker.index];
-              infowindow.setContent(content);
-              infowindow.open(map, marker);
-            }
-          })(marker));
-          markersArray.push(marker);
-        }
+//           google.maps.event.addListener(marker, "click", (function (marker) {
+//             return function () {
+//               console.log(windowsArray[marker.index])
+//               var content = windowsArray[marker.index];
+//               infowindow.setContent(content);
+//               infowindow.open(map, marker);
+//             }
+//           })(marker));
+//           markersArray.push(marker);
+//         }
     
-      });
-})
-// $(document).ready(function () {
-//   // $.get("/api/vendors").then(function (data) {
-//   //   console.log(data);
-//   // });
+//       });
 // })
