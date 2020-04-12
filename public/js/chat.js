28270c66dd7a34b02c5e282c341e9a7b080dc622
toon <nu.chai56@gmail.com>
2020-04-12 04:57:56 -0500

    chat app works, will add users name and update css

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,72 @@
+
 const socket = io()
 
-socket.on('countUpdated', () => {
-    console.log('the count has been updated!')
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+const $msg = document.querySelector('#msg')
+
+// Templates
+const messageTemplate = document.querySelector('#msg-tem').innerHTML
+const locationMessageTemplate = document.querySelector('#loc-msg-tem').innerHTML
+
+
+//message
+socket.on('message', (message) => {
+    console.log(message)
+const html = Mustache.render(messageTemplate,{
+    message,
+    createdAt:  moment(message.createdAt).format('h:m:s a')
+})
+    $msg.insertAdjacentHTML('beforeend', html)
+})
+
+
+//location
+socket.on('locationMessage', (url)=>{
+    console.log(url)
+    const html = Mustache.render(locationMessageTemplate,{
+        url
+    })
+    $msg.insertAdjacentHTML('beforeend', html)
+})
+
+
+
+$messageForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+
+    $messageFormButton.setAttribute('disabled', 'disabled') 
+
+    const message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message, (error)=>{
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+        if(error){
+            return console.log(error)
+        }
+        console.log('Message delivered!')
+    })
+})
+
+$sendLocationButton.addEventListener('click',()=>{
+    if (!navigator.geolocation){
+    return alert('"Geolocation is not supported by your browser.')
+    }
+
+    $sendLocationButton.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition((position)=>{
+            console.log(position)
+            socket.emit('sendLocation', {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            }, ()=>{
+                $sendLocationButton.removeAttribute('disabled')
+                console.log('Location shared!')
+            })
+    })
 })
--- server.js
+++ server.js
@@ -2,55 +2,55 @@
 var express = require("express");
 var session = require("express-session");
 const path = require('path')
-
-
 const http = require("http");
 const socketio = require("socket.io");
+const Filter = require('bad-words')
 const app = express();
 const PORT = process.env.PORT || 8080;
 const server = http.createServer(app);
 const io = socketio(server);
-
-
 const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
 io.on('connection',(socket) => {
-  console.log('welcome')
+  // console.log('welcome')
 
-  socket.emit('countUpdated')
+  socket.emit('message', 'Welcome!')
+
+
+  socket.broadcast.emit('message','A new user has joine')
+
+  socket.on('sendMessage', (message, callback)=>{
+    const filter = new Filter()
+
+    if (filter.isProfane(message)){
+      return callback('Profanity is not allowed!')
+    }
+
+    io.emit('message', message)
+    callback('Delivered');
+  })
+
+  socket.on('sendLocation', (coords, callback)=> {
+    io.emit('locationMessage', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    callback()
+  })
+
+  socket.on('disconnect', ()=>{
+    io.emit('message','A user has left')
+  })
 })
 
-// io.on('connection',(socket)=>{
-//   console.log("Someone connected")
-//   socket.emit('messageFromServer',{data:"Welcome"});
-//   socket.on('messageToServer',(dataFromClient)=>{
-//       console.log(dataFromClient)
-//   })
-//   socket.on('newMessageToServer',(msg)=>{
-//       io.of('/').emit('messageToClients',{text:msg.text})
-//   })
-//   setTimeout(()=>{
-//       io.of('/admin').emit('welcome',"Welcome to the admin channel, from the main channel!")
-//   },2000)
-// })
-
-// io.of('/admin').on('connection',(socket)=>{
-//   console.log("Someone connected to the admid!")
-//   io.of('/admin').emit('welcome',"Welcome to the admin channel!");
-// })
-
+// createdAt: new Date().getTime()
 
 // Requiring passport as we've configured it 
 var passport = require("./config/passport");
 var dotenv = require("dotenv")
 dotenv.config();
 // Setting up port and requiring models for syncing
-// var PORT = process.env.PORT || 9080;
 var db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
-// var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -62,14 +62,12 @@ app.use(passport.session());
 
 // Requiring our routes
 require("./routes/html-routes.js")(app);
-// eslint-disable-next-line no-undef
 require("./routes/api-routes.js")(app);
 
   
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
-  // const expressServer = 
   server.listen(PORT, function() {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
