c2be8f042cbda775d72cf7013f692c51c5d95aea
philipwking <42879808+philipwking@users.noreply.github.com>
2020-04-14 18:06:24 -0500

    inventory inside overlay

--- public/js/map.js
+++ public/js/map.js
@@ -10,48 +10,52 @@ function initMap() {
   $.ajax({
     url: "/api/storefronts",
     method: "GET"
-  }).then(function(results){
-  console.log(results)
+  }).then(function (results) {
+    console.log(results)
     var data = results
-    
-  for (var i = 0; i < data.length; i++) {
-    var storefrontId = data[i].id
-    console.log(data[i].latitude)
-    var icon = {
-      url: "./assets/vendor.png",
-      scaledSize: new google.maps.Size(30, 30),
-      origin: new google.maps.Point(0, 0),
-      anchor: new google.maps.Point(0, 0)
-    };
-    var position = {
-      lat: parseFloat(data[i].latitude),
-      lng: parseFloat(data[i].longitude)
-    }
-    console.log(position)
-    var marker = new google.maps.Marker({
-      position: position,
-      map: map,
-      icon: icon,
-      index: i,
-    })
 
-    google.maps.event.addListener(marker, "click", function (marker) {
-      $("#overlay").css("display","block")
-      var invUrl = "/api/inventory/" + storefrontId
-      console.log(invUrl)
-      $.ajax({
-        url: invUrl,
-        method: "GET"
-      }).then(function(results){
-        res.json(results)
+    for (var i = 0; i < data.length; i++) {
+      var storefrontId = data[i].id
+      console.log(data[i].latitude)
+      var icon = {
+        url: "./assets/vendor.png",
+        scaledSize: new google.maps.Size(30, 30),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(0, 0)
+      };
+      var position = {
+        lat: parseFloat(data[i].latitude),
+        lng: parseFloat(data[i].longitude)
+      }
+      console.log(position)
+      var marker = new google.maps.Marker({
+        position: position,
+        map: map,
+        icon: icon,
+        index: i,
       })
-    })
 
-    markersArray.push(marker);
-  }
+      google.maps.event.addListener(marker, "click", function (marker) {
+        $("#overlay").css("display", "block")
+        var invUrl = "/api/inventory/" + storefrontId
+        console.log(invUrl)
+        $.ajax({
+          url: invUrl,
+          method: "GET"
+        }).then(function (results) {
+          for (var i = 0; i < results.length; i++) {
+            var index = i+1
+            $("#tbody").append(`<tr><td>${index}</td><td>${results[i].productName}</td><td>${results[i].price}</td></tr>`)
+          }
+        })
+      })
 
-})}
+      markersArray.push(marker);
+    }
 
-function closeOverlay(){
-  $("#overlay").css("display","none")
+  })
+}
+
+function closeOverlay() {
+  $("#overlay").css("display", "none")
 }
